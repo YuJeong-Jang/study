8c88079a880def290c7276d2f0683e604ff5d577
YuJeong-Jang <aridoasis@naver.com>
2023-12-20 21:01:28 +0900

    separate branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:study_netflix/screen/home_screen.dart';
-import 'package:study_netflix/screen/more_screen.dart';
-import 'package:study_netflix/widget/bottom_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,36 +16,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'jayFlix',
-        theme: ThemeData(
-            brightness: Brightness.dark,
-            primaryColor: Colors.black,
-            colorScheme: ColorScheme.fromSeed(
-                seedColor: Colors.white, brightness: Brightness.dark)),
-        home: DefaultTabController(
-          length: 4,
-          child: Scaffold(
-              body: TabBarView(
-                physics: NeverScrollableScrollPhysics(),
-                children: <Widget>[
-                  HomeScreen(),
-                  // Container(
-                  //   child: Center(child: Text('home')),
-                  // ),
-                  Container(
-                    child: Center(child: Text('search')),
-                  ),
-                  Container(
-                    child: Center(child: Text('save')),
-                  ),
-                  MoreScreen()
-                  // Container(
-                  //   child: Center(child: Text('more')),
-                  // )
-                ],
-              ),
-              bottomNavigationBar: Bottom()),
-        ));
+    return MaterialApp();
   }
 }
